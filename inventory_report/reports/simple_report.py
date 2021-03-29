bf9bde6f9f2244d8b8cdddb2ef92a52155268e5f
ederusena <eder.sena@live.com>
2021-03-29 19:33:35 -0300

    chore: Requisito 1 pronto

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from statistics import mode
 
 
 class SimpleReport:
@@ -7,17 +8,34 @@ class SimpleReport:
 
     @classmethod
     def generate(cls, list):
+        hoje = datetime.today()
+        hojeF = f"{hoje.year}-0{hoje.month}-{hoje.day}"
+
         data_de_fabricacao = min(
             [
-                datetime.strftime(
+                datetime.strptime(
                     data["data_de_fabricacao"], "%Y-%m-%d"
                 ).date()
                 for data in list
             ]
         )
 
+        data_de_vencimento = min(
+            [
+              datetime.strptime(data["data_de_validade"], "%Y-%m-%d").date()
+              for data in list
+              if datetime.strptime(data["data_de_validade"], "%Y-%m-%d")
+              .date()
+              > datetime.strptime(hojeF, "%Y-%m-%d").date()
+            ]
+        )
+
+        prod_stock = mode([data["nome_da_empresa"] for data in list])
+
         relatorio = (
             f"Data de fabricação mais antiga: {data_de_fabricacao}\n"
+            f"Data de validade mais próxima: {data_de_vencimento}\n"
+            f"Empresa com maior quantidade de produtos estocados: {prod_stock}\n"
         )
 
         return relatorio
