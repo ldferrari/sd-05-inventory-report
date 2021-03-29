6e4bcc20e3760558164ee4e2951be2995d8bdc86
Christian Duessel <duessel@gmail.com>
2021-03-29 19:53:14 -0300

    req2

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,21 @@
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, list):
+        report_p1 = SimpleReport.generate(list)
+        companies = [
+            co["nome_da_empresa"] for co in list
+        ]
+        counting = Counter(companies)
+        co_stock = ''
+        for cia in counting:
+            co_stock += f"- {cia}: {counting[cia]}\n"
+
+        return (
+            f"{report_p1}\n"
+            f"Produtos estocados por empresa: \n"
+            f"{co_stock}"
+        )
