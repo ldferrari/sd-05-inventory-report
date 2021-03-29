9ee99e8e695984c37076afd154f412e2866ab138
Christian Duessel <duessel@gmail.com>
2021-03-29 19:18:36 -0300

    req1

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,26 @@
+from datetime import date
+from collections import Counter
+
+
+class SimpleReport:
+    @classmethod
+    def generate(cls, list):
+        today = date.today().isoformat()
+        oldest_fab_date = min([old["data_de_fabricacao"] for old in list])
+        next_fab_date = min(
+            [
+                close["data_de_validade"]
+                for close in list
+                if close["data_de_validade"] > today
+            ]
+        )
+        companies = [
+            co["nome_da_empresa"] for co in list
+        ]
+        counting = Counter(companies)
+        bs = max(counting)
+        return (
+            f"Data de fabricação mais antiga: {oldest_fab_date}\n"
+            f"Data de validade mais próxima: {next_fab_date}\n"
+            f"Empresa com maior quantidade de produtos estocados: {bs}\n"
+        )
