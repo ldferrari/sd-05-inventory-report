35e538bb8aa3d7d933050d484f0dde1f99258e03
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2021-03-29 20:43:46 -0300

    prepared importer of req6

--- inventory_report/importer/importer.py
+++ inventory_report/importer/importer.py
@@ -1,1 +1,11 @@
-# Importer abstract class here
+from abc import ABC, abstractmethod
+
+
+class Importer(ABC):
+    @abstractmethod
+    def import_data(self, filepath):
+        raise NotImplementedError
+
+# follow examples of where to find model synthax
+# https://app.betrybe.com/course/computer-science/poo/oo-na-pratica/solutions
+# https://docs.python.org/3/library/abc.html
