a0852ca9bb3d4166b6737a8972e405404464686f
Felipe Vieira <flpvieira86@gmail.com>
2021-03-29 13:55:59 -0300

    req 4 ok

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import json
 from ..reports.simple_report import SimpleReport
 from ..reports.complete_report import CompleteReport
 
@@ -7,8 +8,12 @@ from ..reports.complete_report import CompleteReport
 class Inventory:
     @classmethod
     def import_data(cls, path, report_type):
+
+        file_path, file_extension = os.path.splitext(path)
+        print('\nFILE PATH', file_path)
+        print('FILE_EXTENSION', file_extension)
         with open(path) as file:
-            data = cls.load_file(file)
+            data = cls.load_file(cls, file_extension, file)
 
             if report_type == "simples":
                 return SimpleReport.generate(data)
@@ -17,8 +22,11 @@ class Inventory:
                 return CompleteReport.generate(data)
 
     @staticmethod
-    def load_file(file):
-        data = csv.DictReader(file)
-        data_list = list(data)
-
-        return data_list
+    def load_file(cls, file_extension, file):
+        data = []
+        if file_extension == ".csv":
+            file_data = csv.DictReader(file)
+            data = list(file_data)
+        if file_extension == ".json":
+            data = json.load(file)
+        return data
