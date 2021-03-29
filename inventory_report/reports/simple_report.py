8196507561d6c80cbfdfca1af1451938e92a3d5c
ederusena <eder.sena@live.com>
2021-03-29 18:49:09 -0300

    chore: Requisito 1, data de fabricacao

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,8 +1,23 @@
-import datetime
+from datetime import datetime
 
 
 class SimpleReport:
-    def generate(arquivo):
-        print("Aee")
-        return "String formatada como relatorio"
+    def __init__(self):
+        pass
 
+    @classmethod
+    def generate(cls, list):
+        data_de_fabricacao = min(
+            [
+                datetime.strftime(
+                    data["data_de_fabricacao"], "%Y-%m-%d"
+                ).date()
+                for data in list
+            ]
+        )
+
+        relatorio = (
+            f"Data de fabricação mais antiga: {data_de_fabricacao}\n"
+        )
+
+        return relatorio
