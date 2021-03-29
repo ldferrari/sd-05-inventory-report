b8975cba37ac65d17c82f0227b5827d66786be72
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-03-29 17:44:57 -0300

    add csv and json data getter

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,49 @@
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+import csv
+import json
+
+
+def get_csv_stock(path):
+    try:
+        with open(path) as file:
+            reader = csv.reader(file, delimiter=",")
+
+            header, *data = reader
+
+            stock_Jsoned = [
+                {header[i]: info[i] for i in range(len(info))} for info in data
+            ]
+
+            return stock_Jsoned
+
+    except FileNotFoundError:
+        raise ValueError(f"Arquivo {path} não encontrado")
+
+
+def get_json_stock(path):
+    with open(path) as file:
+        return json.load(file)
+
+
+class Inventory:
+    @classmethod
+    def import_data(cls, path, report_type):
+
+        if ".csv" in path:
+            stock = get_csv_stock(path)
+        if ".json" in path:
+            stock = get_json_stock(path)
+
+        if report_type == "simples":
+            report = SimpleReport.generate(stock)
+        else:
+            report = CompleteReport.generate(stock)
+
+        return report
+
+
+# print(Inventory.import_data("inventory_report/data/inventory.csv", "completo"))
+print(
+    Inventory.import_data("inventory_report/data/inventory.json", "simples")
+)
