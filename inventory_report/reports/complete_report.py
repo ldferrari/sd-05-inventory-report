0197a2a0833b75acf7bf7b7e00e8548ae2ee1304
ederusena <eder.sena@live.com>
2021-03-29 21:14:55 -0300

    chore: requisito 2

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,27 @@
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, list):
+        simplereport = super().generate(list)
+
+        empresas = [data["nome_da_empresa"] for data in list]
+        empresas_contadas = Counter(empresas)
+        # ({'sanofi-aventis U.S. LLC': 2, 'Forces of Nature': 1, 'Newton Laboratories': 1})
+        # - Physicians Total Care, Inc.: QUANTIDADE
+        # empresas_contadas['sanofi-aventis U.S. LLC'] = 2
+        filtered_counter = []
+        for nome in empresas_contadas:
+            filtered_counter.append(f"- {nome}: {empresas_contadas[nome]}")
+
+        relatorio = (
+                f"{simplereport}\n"
+                f"Produtos estocados por empresa: \n"
+                f"{filtered_counter[0]}\n"
+                f"{filtered_counter[1]}\n"
+                f"{filtered_counter[2]}\n"
+            )
+
+        return relatorio
