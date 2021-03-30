53a3bccff668da5133c46aa47ecbddf2dcf70228
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2021-03-30 15:30:07 -0300

    req5 CsvImporter blind

--- inventory_report/importer/csv_importer.py
+++ inventory_report/importer/csv_importer.py
@@ -1,8 +1,23 @@
-# import csv
-# from inventory_report.importer.importer import Importer
+import csv
+from inventory_report.importer.importer import Importer
 
 
-# class CsvImporter(Importer):
-#     @classmethod
-#     import in csv format and then return data
-#     launch exception if not expected format
+class CsvImporter(Importer):
+    @classmethod
+    def import_data(cls, filepath):
+        try:
+            assert ".csv" in filepath
+            # assert filepath.endswith(".csv"), other option
+            with open(filepath) as file:
+                csv_reader = csv.DictReader(file, delimiter=",")
+                csv_data = []
+                for product in csv_reader:
+                    csv_data.append(product)
+                return csv_data
+        except AssertionError:
+            raise ValueError("Formato invalido")
+        except FileNotFoundError:
+            raise ValueError("Arquivo não encontrado")
+
+# from own former project csv example:
+# https://github.com/tryber/sd-05-tech-news/pull/10/files
