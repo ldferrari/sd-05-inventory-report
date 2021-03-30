a8f0c381a6a74630cbbeb7de9fd24798803883bc
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-03-30 02:41:50 -0300

    Requisito 8 - Obrigado Luciano @ramalho e Nilo @lskbr por escreverem livros de python, vcs sao uns lindos

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,23 @@
+import sys
+from os import path
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        return print("Verifique os argumentos", file=sys.stderr)
+    else:
+        _, file_path, report_type = sys.argv
+        importer = {
+            ".csv": CsvImporter,
+            ".xml": XmlImporter,
+            ".json": JsonImporter,
+        }
+        _, ext = path.splitext(file_path)
+
+        report = InventoryRefactor(importer[ext])
+        report = report.import_data(file_path, report_type)
+        print(report, end="")
