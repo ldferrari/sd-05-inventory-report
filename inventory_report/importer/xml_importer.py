dfc3e708fff363dd89c06d7fd571ea20c5880ffa
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2021-03-30 16:53:10 -0300

    req5 XmlImporter tested

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -6,20 +6,28 @@ from inventory_report.importer.importer import Importer
 class XmlImporter(Importer):
     @classmethod
     def import_data(cls, filepath):
-        if not filepath.endswith(".csv"):
+        # no try except structure because of flake complexity error
+        if not filepath.endswith(".xml"):
             raise ValueError("Formato invalido")
         tree = ET.parse(filepath)
         root = tree.getroot()
-        print(f"root: {root}")
+        # print(f"root: {root}")
+        # root: <Element 'dataset' at 0x7ff34e4d9d60>
         xml_data = []
-        # some iteration
+        for element in root:
+            each_product = {}
+            for item in element:
+                each_product[item.tag] = item.text
+            # print(f"each_product: {each_product}")
+            xml_data.append(each_product)
         return xml_data
         # except FileNotFoundError:
         #     raise ValueError("Arquivo não encontrado")
 
 
 if __name__ == "__main__":
-    print(XmlImporter.import_data('inventory_report/data/inventory.csv'))
+    print(XmlImporter.import_data('inventory_report/data/inventory.xml'))
 
 # https://stackabuse.com/reading-and-writing-xml-files-in-python/
 # https://docs.python.org/3/library/xml.etree.elementtree.html
+# other possibility: xmltodict https://docs.python-guide.org/scenarios/xml/
