2da7665696352623ef01979e05be14258c5c8539
Lucascv <lucascv333@gmail.com>
2021-03-30 18:13:37 -0300

    teste req xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -30,14 +30,32 @@ def read_json(file_path):
         raise ValueError("File not found")
 
 
+def read_xml(file_path):
+    try:
+        with open(file_path) as file:
+            ler = xmltodict.parse(file.read())
+            print(type(json.dumps(ler)))
+            data = json.loads(json.dumps(ler))['dataset']['record']
+            return data
+    except FileNotFoundError:
+        raise ValueError("File not found")
+
+
+def create_data(file_path):
+    if file_path.split('.')[1] == 'csv':
+        report = read_csv(file_path)
+        return report
+    if file_path.split('.')[1] == 'json':
+        report = read_json(file_path)
+        return report
+    if file_path.split('.')[1] == 'xml':
+        report = read_xml(file_path)
+        return report
+
 class Inventory:
     @classmethod
     def import_data(cls, file_path, report_type):
-        report = ""
-        if file_path.split('.')[1] == 'csv':
-            report = read_csv(file_path)
-        if file_path.split('.')[1] == 'json':
-            report = read_json(file_path)
+        report = create_data(file_path)
 
         if (report_type == "simples"):
             return SimpleReport.generate(report)
