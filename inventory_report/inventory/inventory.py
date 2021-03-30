90c50e962044bdf35cb39595ffb20b4da20cba17
luiz-avelar <luiz.avelar25@gmail.com>
2021-03-30 19:23:08 -0300

    Requirement 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,9 @@
+# Referência
+# https://docs.python.org/3/library/xml.etree.elementtree.html
+
 import csv
 import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -15,10 +19,23 @@ class Inventory:
             data = json.load(file)
             return data
 
+    def read_xml(filepath):
+        tree = ET.parse(filepath)
+        root = tree.getroot()
+        data = []
+        for record in root:
+            product = {}
+            for tag in record:
+                product[tag.tag] = tag.text
+            data.append(product)
+        return data
+
     @classmethod
     def import_data(self, filepath, report_type):
         if filepath.endswith(".csv"):
             data = self.read_csv(filepath)
+        elif filepath.endswith(".xml"):
+            data = self.read_xml(filepath)
         else:
             data = self.read_json(filepath)
         if report_type == "simples":
