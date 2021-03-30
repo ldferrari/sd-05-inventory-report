56ea9b99e430ee5a60fcea5129a0654860d9b70b
ederusena <eder.sena@live.com>
2021-03-30 14:37:39 -0300

    fix: Requisito 2, relatorio na forma de string

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -6,21 +6,32 @@ class CompleteReport(SimpleReport):
     # Always use cls for the first argument to class methods.
     @classmethod
     def generate(cls, list):
+        # Herda metodo generate da classe SimpleReport
         simplereport = super().generate(list)
 
+        # Lista de compreensão -  coloca todos itens
+        # "nome_da_empresa" na lista empresas
         empresas = [data["nome_da_empresa"] for data in list]
+
+        # Counter agrupa ocorrencias {"empresa_a", "2"}
         empresas_contadas = Counter(empresas)
 
         filtered_counter = []
         for nome in empresas_contadas:
             filtered_counter.append(f"- {nome}: {empresas_contadas[nome]}")
 
+        # Ler o array e formata em uma unica string
+        # Para facilitar o relatório
+        completereport = ""
+        for company in empresas_contadas:
+            completereport += (
+                f"- {company}: {empresas_contadas[company]}\n"
+            )
+
         relatorio = (
                 f"{simplereport}\n"
                 f"Produtos estocados por empresa: \n"
-                f"{filtered_counter[0]}\n"
-                f"{filtered_counter[1]}\n"
-                f"{filtered_counter[2]}\n"
+                f"{completereport}"
             )
 
         return relatorio
