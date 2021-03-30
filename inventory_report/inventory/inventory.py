f9b3a3e343782df96d54d232c020ca3e53c59e93
Christian Duessel <duessel@gmail.com>
2021-03-30 15:48:27 -0300

    req 4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,21 +1,27 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
     @classmethod
     def import_data(cls, filepath, type):
         try:
-            assert filepath.endswith('.csv')
-            with open(filepath) as file:
-                arquivo = csv.DictReader(file, delimiter=',')
-                # print(arquivo)
-                # print("oi eu aqui")
-                product_list = [item for item in arquivo]
-                # print(product_list)
-        except AssertionError:
-            raise ValueError("Formato invalido")
+            if filepath.endswith('.csv'):
+                with open(filepath) as file:
+                    arquivo = csv.DictReader(file, delimiter=',')
+                    # print(arquivo)
+                    # print("oi eu aqui")
+                    product_list = [item for item in arquivo]
+                    # print(product_list)
+            elif filepath.endswith('.json'):
+                with open(filepath) as file:
+                    reader = file.read()
+                    product_list = json.loads(reader)
+                    # print(arquivo)
+                    # print("oi eu aqui")
+                    # print(product_list)
         except FileNotFoundError:
             raise ValueError(f'Arquivo {filepath} não encontrado')
         else:
