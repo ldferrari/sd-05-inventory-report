cd38b91c1e6cdd131a669b79ba8e1a7a26b02f93
Andrevlc <andrevezi@gmail.com>
2021-03-30 19:23:03 -0300

    Requisite 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ import csv
 import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
+import xml.etree.ElementTree as ET
 
 
 def read_json(file):
@@ -14,14 +15,32 @@ def read_csv(file):
         csv_data = csv.DictReader(file)
         return list(csv_data)
 
+# https://docs.python.org/3/library/xml.etree.elementtree.html
+
+
+def read_xml(file):
+    root = ET.parse(file).getroot()
+    tree = []
+    for record in root:
+        dict_format = {}
+        for tag in record:
+            dict_format[tag.tag] = tag.text
+        tree.append(dict_format)
+    print(tree)
+    return tree
+
 
 class Inventory:
     @classmethod
     def import_data(self, filepath, type):
         if filepath.endswith(".csv"):
             data = read_csv(filepath)
-        if filepath.endswith(".json"):
+        elif filepath.endswith(".json"):
             data = read_json(filepath)
+        elif filepath.endswith(".xml"):
+            data = read_xml(filepath)
+        else:
+            raise ValueError("Arquivo inválido")
         if type == 'completo':
             report = CompleteReport.generate(data)
         else:
