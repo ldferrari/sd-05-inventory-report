bb8ff822760285b7d9a4f9d4c83bcaad20b3e4aa
Rafael Assad <rafaassadeng@gmail.com>
2021-03-31 15:48:19 -0300

    req 1

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,24 @@
-import datetime
+from datetime import date
 from collections import Counter
+
+
+class SimpleReport:
+    @classmethod
+    def generate(self, list):
+        nowadays = date.today().isoformat()
+        old_fab = min([old]["data_de_fabricacao"] for old in list)
+        near_expire = min(
+            [
+                near["data_de_validade"]
+                for near in list
+                if near["data_de_validade"] > nowadays
+            ]
+        )
+        company = [comp["nome_da_empresa"] for comp in list]
+        count = Counter(company)
+        big_st = max(count)
+        return (
+            f"Data de fabriação mais antiga: {old_fab}\n"
+            f"Validade mais próxima: {near_expire}\n"
+            f"Empresa com maior quantidade de produtos estocados: {big_st}\n"
+        )
