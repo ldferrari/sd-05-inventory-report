fd37f8f5b7d89b647180823169a71143809bca96
Rafael Assad <rafaassadeng@gmail.com>
2021-03-31 17:08:06 -0300

    req 4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,19 +1,27 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
 
-def read_data(filepath):
+def read_data_csv(filepath):
     with open(filepath) as file:
         csv_data = csv.DictReader(file)
         return list(csv_data)
+
+
+def read_data_json(filepath):
+    with open(filepath) as file:
+        return json.load(file)
 
 
 class Inventory:
     @classmethod
     def import_data(self, test, type):
         if test.endswith(".csv"):
-            data = read_data(test)
+            data = read_data_csv(test)
+        if test.endswith(".json"):
+            data = read_data_json(test)    
         if type == 'completo':
             report = CompleteReport.generate(data)
         else:
