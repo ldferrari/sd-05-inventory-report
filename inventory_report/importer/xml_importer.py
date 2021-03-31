d73a2f8a86c188c68f0f767dfcff55b10ef3620a
virginia-duca <virginiambd.bio@gmail.com>
2021-03-31 17:51:15 -0300

    flake

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -20,7 +20,9 @@ class XmlImporter(Importer):
                     "id": record.find("id").text,
                     "nome_do_produto": record.find("nome_do_produto").text,
                     "nome_da_empresa": record.find("nome_da_empresa").text,
-                    "data_de_fabricacao": record.find("data_de_fabricacao").text,
+                    "data_de_fabricacao": record.find(
+                      "data_de_fabricacao"
+                    ).text,
                     "data_de_validade": record.find("data_de_validade").text,
                     "numero_de_serie": record.find("numero_de_serie").text,
                     "instrucoes_de_armazenamento": record.find(
--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -4,6 +4,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.importer.json_importer import JsonImporter
 from inventory_report.importer.xml_importer import XmlImporter
 
+
 class Inventory:
 
     # Criar uma function que recebe o tipo e formato do relatorio
