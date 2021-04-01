295677649573dbab21add5139312fc7e4538eddf
ferrari <luisdiasferrari@gmail.com>
2021-04-01 01:51:54 -0300

    requirement 3

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -11,15 +11,13 @@ class Inventory():
         with open(list, "r") as date:
             reader_csv = csv.DictReader(date, delimiter=",")
             for company in reader_csv:
-              result_csv.append(company)
-            print(result_csv)
+                result_csv.append(company)
             return result_csv
 
     @classmethod
     def import_data(cls, list, report):
-      date = cls.edit_csv(list)
-      result = ""
-      if report == "simples":
-        return SimpleReport.generate(date)
-      if report == "completo":
-        return CompleteReport.generate(date)
+        date = cls.edit_csv(list)
+        if report == "simples":
+            return SimpleReport.generate(date)
+        if report == "completo":
+            return CompleteReport.generate(date)
