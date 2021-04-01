e265b670bde3567d942cf606af322a0e59923bf6
ferrari <luisdiasferrari@gmail.com>
2021-04-01 03:06:02 -0300

    requirement 4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,6 @@
 import csv
+import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -15,8 +17,20 @@ class Inventory():
             return result_csv
 
     @classmethod
+    def edit_json(cls, list):
+        result_json = []
+        with open(list, "r") as date:
+            reader_json = json.load(date)
+            for company in reader_json:
+                result_json.append(company)
+            return result_json
+
+    @classmethod
     def import_data(cls, list, report):
-        date = cls.edit_csv(list)
+        if list.endswith(".csv"):
+            date = cls.edit_csv(list)
+        if list.endswith(".json"):
+            date = cls.edit_json(list)
         if report == "simples":
             return SimpleReport.generate(date)
         if report == "completo":
