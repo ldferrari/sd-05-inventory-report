e2907d33d38c3b1a1ee767368b46ee6dfa260ae8
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2021-04-06 21:13:24 -0300

    corrigido

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -5,20 +5,20 @@ from inventory_report.reports.complete_report import CompleteReport
 
 
 class InventoryRefactor(Iterable):
-    def __init__(self, aggregate):
-        self.estoque = []
-        self.aggregate = aggregate
+    def __init__(self, importer):
+        self.data = []
+        self.importer = importer
 
     def __iter__(self):
-        return InventoryIterator(self.estoque)
+        return InventoryIterator(self.data)
 
     def import_data(self, filepath, tipo):
-        self.estoque += self.aggregate.import_data(filepath)
+        self.data.extend(self.importer.import_data(filepath))
 
         if (tipo == "simples"):
-            return SimpleReport.generate(self.estoque)
+            return SimpleReport.generate(self.data)
 
-        return CompleteReport.generate(self.estoque)
+        return CompleteReport.generate(self.data)
 
 #   https://stackoverflow.com/questions/9884132/what-exactly-are-iterator-iterable-and-iteration
 #   https://docs.python.org/3/library/collections.abc.html
